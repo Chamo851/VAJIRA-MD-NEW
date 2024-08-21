30ad3979fa1d334790404a4c7ca92f75506d5b7a
Chamo851 <chamodimax0@gmail.com>
2024-08-21 20:59:39 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
 global.thumb = fs.readFileSync('./lib/bugs/venom.jpg')
 const BOTNAME = 'VAJIRA-MD|-WA-BOT™';
 const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
-global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
+global.owner = process.env.OWNER_NUMBER  || '94706200559' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=KclmWaDZ#mBqaqrqNI5UUc7mgUZFUqjaaw2zHI4ojBuQEs0HWImM' ,
